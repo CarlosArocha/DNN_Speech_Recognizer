26bd2d854c4dcb2266bc7c5c1535e8d9464e1d01
Carlos Arocha <carloslarochas@gmail.com>
2022-07-13 16:52:11 +0100

    Finally succesfully convertion from flac to wav with miniaudio library

--- vui.py
+++ vui.py
@@ -4,6 +4,8 @@ import json
 import os
 import numpy as np
 import soundfile as sf
+import miniaudio
+import array
 
 def json_to_local(desc_file='train_corpus.json',
                   new_file='train_corpus_local.json'):
@@ -25,50 +27,64 @@ def json_to_local(desc_file='train_corpus.json',
 
     print('New file ready')
 
-def flac_to_wav(data_directory):
-    '''wav_file_count = 0
-    save_path = os.path.join(data_directory, group, speaker, chapter)
-    idtag = speaker+'-'+chapter
-    transcript_filename = idtag + '.trans.txt'
-    os.makedirs(save_path, exist_ok=True)
-    outfile = open(os.path.join(save_path, transcript_filename), 'w')
-    save_path = os.path.join(data_directory, group, speaker, chapter)
-    for file in os.listdir(input_directory):
-        if file.endswith(".wav"):
-            data, samplerate = sf.read(os.path.join(input_directory,file))
-            sf.write('testwavout.wav',data,samplerate)
-            # save the file to its new place
-            ident = idtag + '-' + '{:04d}'.format(wav_file_count)
-            new_filename = ident+'.wav'
-            print(ident)
-            os.replace('testwavout.wav',os.path.join(save_path,new_filename))
-            wav_file_count += 1
-            outfile.write(ident+' \n')
-    outfile.close()'''
+def play_back_file(filename):
+    stream = miniaudio.stream_file(filename)
+    device = miniaudio.PlaybackDevice()
+    device.start(stream)
+    input("Audio file playing in the background. Enter to stop playback: ")
+    device.close()
+
+def flac_to_wav_wminiaudio(filename, new_file):
+
+    src = miniaudio.decode_file(filename, dither=miniaudio.DitherMode.TRIANGLE)
+    # In case you want to print the source file info: decomment
+    # print("Source: ", src)
+    result = miniaudio.DecodedSoundFile("result", src.nchannels,
+                                        src.sample_rate, src.sample_format,
+                                        array.array('b'))
+    converted_frames = miniaudio.convert_frames(src.sample_format,
+                                                src.nchannels, src.sample_rate,
+                                                src.samples.tobytes(),
+                                                result.sample_format,
+                                                result.nchannels,
+                                                result.sample_rate)
+    # note: currently it is not possible to provide a dithermode
+    # to convert_frames()
+    result.num_frames = int(len(converted_frames) / \
+                                 result.nchannels / \
+                                 result.sample_width)
+    result.samples.frombytes(converted_frames)
+    miniaudio.wav_write_file(new_file, result)
+    # In case you want to print the output file info: decomment
+    # output_info = miniaudio.get_file_info(new_file)
+    # print(output_info)
+
+
+def convert_flac_files(data_directory):
 
     for group in os.listdir(data_directory):
         if not group.startswith('.'):
             group_folder = os.path.join(data_directory, group)
             for speaker in os.listdir(group_folder):
                 if not speaker.startswith('.'):
-                    speaker_folder = os.path.join(data_directory, group, speaker)
+                    speaker_folder = os.path.join(data_directory, group,
+                                                  speaker)
                     for file in os.listdir(speaker_folder):
-                        if (not file.startswith('.')) and file.endswith('.flac'):
+                        if (not file.startswith('.')) and \
+                                file.endswith('.flac'):
                             filename = os.path.join(speaker_folder, file)
-                            print(filename)
-                            data, samplerate = sf.read(os.path.join(speaker_folder, file))
-                            sf.write(file[:-4]+'wav', data, samplerate, 'PCM_16')
+                            new_file = file[:-4]+'wav'
+                            flac_to_wav_wminiaudio(filename,
+                                        os.path.join(speaker_folder, new_file))
+                # When sounfile library is fixed use:
+                # data, samplerate = sf.read(os.path.join(speaker_folder, file))
+                # sf.write(new_file, data, samplerate, 'PCM_16')
 
     print('.flac files converted to .wav')
 
 wk_directory='/Users/carlos/Dropbox/AI/GITHUB/UDACITY/NLP/DNN_Speech_Recognizer'
 os.chdir(wk_directory)
 
-# json_to_local()
-
-print(sf.available_formats())
-print(sf.available_subtypes('FLAC'))
-print(sf._libname)
 
 directory = '/Volumes/OutSSD/DATA/NLP/LibriSpeech/dev-clean'
-flac_to_wav(directory, )
+convert_flac_files(directory, )
